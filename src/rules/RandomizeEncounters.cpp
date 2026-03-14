36f2fcd5e064cde975978471f1aaad1faa135abd
andrewmac <andrewmac@gmail.com>
2026-03-13 22:49:06 -0300

    Fix HP/MP not always being full in Randomize Encounters.

--- src/rules/RandomizeEncounters.cpp
+++ src/rules/RandomizeEncounters.cpp
@@ -483,9 +483,9 @@ void RandomizeEncounters::generateEnemyStatMultipliers()
         StatMultiplierSet enemySet;
 
         enemySet.currentHP  = dist(rng);
-        enemySet.maxHP      = dist(rng);
+        enemySet.maxHP      = enemySet.currentHP;
         enemySet.currentMP  = dist(rng);
-        enemySet.maxMP      = dist(rng);
+        enemySet.maxMP      = enemySet.currentMP;
         enemySet.strength   = dist(rng);
         enemySet.magic      = dist(rng);
         enemySet.evade      = dist(rng);
